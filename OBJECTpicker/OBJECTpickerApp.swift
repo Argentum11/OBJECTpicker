87b4d78904d70c491419bc39b24836642e4f3d4c
leoChang <leochang@gmail.com>
2021-11-29 16:45:28 +0800

    Initial Commit

---
+++ OBJECTpicker/OBJECTpickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBJECTpickerApp.swift
+//  OBJECTpicker
+//
+//  Created by User16 on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct OBJECTpickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
